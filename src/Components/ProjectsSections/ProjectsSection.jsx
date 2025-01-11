c52922177ab4491df49ad81f15ab821a8cda73cd
Andrey Gabriel <168058295+slicedcheese1@users.noreply.github.com>
2025-01-11 14:56:37 -0400

    centralização sessão de ferramentas

--- src/Components/ProjectsSections/ProjectsSection.jsx
+++ src/Components/ProjectsSections/ProjectsSection.jsx
@@ -1,37 +1,14 @@
 import './ProjectsSection.css';
+import OticaPainel from '../../images/OticaPainel.jpg';
 
 function ProjectsSection() {
   const projects = [
     {
       id: 1,
-      imgUrl: '/Images/OticaPainel.jpg',
-      title: 'Project 1',
-      description: 'Breve descrição sobre o projeto 1. Clique para saber mais.',
+      imgUrl: OticaPainel,
+      title: 'Lunari ERP',
+      description: 'Lunari é um Software integrado responsável pelo Controle de Estoque, Vendas e Finanças. ',
       lastUpdated: '3 mins ago',
-      link: 'https://www.example.com',
-    },
-    {
-      id: 2,
-      imgUrl: 'https://via.placeholder.com/350x200?text=Project+2',
-      title: 'Project 2',
-      description: 'Breve descrição sobre o projeto 2. Clique para saber mais.',
-      lastUpdated: '5 mins ago',
-      link: 'https://www.example.com',
-    },
-    {
-      id: 3,
-      imgUrl: 'https://via.placeholder.com/350x200?text=Project+3',
-      title: 'Project 3',
-      description: 'Breve descrição sobre o projeto 3. Clique para saber mais.',
-      lastUpdated: '10 mins ago',
-      link: 'https://www.example.com',
-    },
-    {
-      id: 4,
-      imgUrl: 'https://via.placeholder.com/350x200?text=Project+4',
-      title: 'Project 4',
-      description: 'Breve descrição sobre o projeto 4. Clique para saber mais.',
-      lastUpdated: '20 mins ago',
       link: 'https://www.example.com',
     },
   ];
